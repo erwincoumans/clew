262ca8f7ac36483e70d9e6da8b8fe36e5dd9a660
Erwin Coumans <erwincoumans@Erwins-MacBook-Pro.local>
2013-05-20 15:24:19 -0700

    fix Mac OSX build

--- src/clewTest.cpp
+++ src/clewTest.cpp
@@ -6,14 +6,18 @@
 
 int main(int argc, char* argv[])
 {
+#ifdef __APPLE__
+const char* cl = "/System/Library/Frameworks/OpenCL.framework/Versions/Current/OpenCL";
+#else
 	const char* cl = "libOpenCL.so.1";
 	const char* cl2 = "libOpenCL.so";
 	const char* idc = "/etc/OpenCL/vendors/amdocl32.icd";
+#endif
 	int result = clewInit(cl);
 	if (result!=CLEW_SUCCESS)
 		printf("clewInit failed with error code %d\n",result);
 	else
-		printf("clewInit succesfull\n");
+		printf("clewInit succesfull using %s\n",cl);
 	return 0;
 }
 
